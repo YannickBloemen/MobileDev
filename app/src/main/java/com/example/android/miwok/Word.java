5c36f2a02a4f7429a4b4b16386b3971f44a8848b
YannickBloemen <yannick.bloemen@student.pxl.be>
2017-10-03 13:31:13 +0200

    Grafisch gedeelte afgemaakt

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,10 +8,18 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId = -1;
 
     public Word(String mDefaultTranslation, String mMiwokTranslation) {
         this.mDefaultTranslation = mDefaultTranslation;
         this.mMiwokTranslation = mMiwokTranslation;
+
+    }
+
+    public Word(String mDefaultTranslation, String mMiwokTranslation,int imageResourceId) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mImageResourceId = imageResourceId;
     }
 
     public String getmDefaultTranslation() {
@@ -20,5 +28,9 @@ public class Word {
 
     public String getmMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,10 +1,59 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by yannick on 2-10-2017.
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
+
+    int backgroundColorId;
+    public WordAdapter(Activity context, ArrayList<Word> words, int backgroundColorId) {
+        super(context, 0, words);
+        this.backgroundColorId = backgroundColorId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        miwokTextView.setText(currentWord.getmDefaultTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        defaultTextView.setText(currentWord.getmMiwokTranslation());
+        if (currentWord.getmImageResourceId() != -1){
+            ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+            iconView.setImageResource(currentWord.getmImageResourceId());
+        }
+        else{
+            ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+            iconView.setVisibility(View.GONE);
+        }
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(),backgroundColorId);
+        textContainer.setBackgroundColor(color);
+
+        return listItemView;
+    }
 }
